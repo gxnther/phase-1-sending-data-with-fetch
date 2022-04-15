3c71680725792d2c9f9d445e1f7fa77ad38920fb
Gunther <gunt.valentine@gmail.com>
2022-04-15 15:20:22 -0400

    commit tree 42dab3d9440a00dbb6c7ac6659b1ec51b0c18d4d

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Add your code here
+function submitData(names, emails) {
+fetch("http://localhost:3000/users", {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+  },
+  body: JSON.stringify({
+    name: `${names}`,
+    email: `${emails}`
+  }),
+})
+.then(resp => resp.json())
+.then(obj => document.body.innerHTML = obj.id)
+.catch(function (error) { 
+    console.log(error.message)
+    document.body.innerText = error.message
+});
+}
+
+// function appendDom(user) {
+//     let users = document.createElement('p') 
+//     users.innerText = `${user.name}   ${user.email}`
+//     document.querySelector("body").appendChild(users)
+// }
